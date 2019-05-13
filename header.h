af1aab2429fe5289ef809c5b6c688f88f83d02f1
Elias <iliecrasmaru@yahoo.com>
2019-05-13 11:00:45 +0300

    Update header.h

--- header.h
+++ header.h
@@ -8,7 +8,7 @@ int verificare_robot_inregistrare();
 int intrebare_logare();
 int resetare_parola();
 int scanare();
-int verificare_parola();
+int verificare_parola(int pass);
 int baza_date,i;
 int n = 0;
 struct date{
