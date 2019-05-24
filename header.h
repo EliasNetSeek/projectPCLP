b4f82df87fe0f0e059d65ac5cd2d421fee4ef9ca
Elias <iliecrasmaru@yahoo.com>
2019-05-24 08:22:29 +0300

    Update header.h

--- header.h
+++ header.h
@@ -1,14 +1,14 @@
 #ifndef HEADER_H_
 #define HEADER_H_
 
-int creare_cont();
-int creare_baza_date();
-int logare_cont();
-int verificare_robot_inregistrare();
-int intrebare_logare();
-int resetare_parola();
+int creare_cont(void);
+int creare_baza_date(void);
+int logare_cont(void);
+int verificare_robot_inregistrare(void);
+int intrebare_logare(void);
+int resetare_parola(void);
 int scanare();
-int stergere_baza_date();
+int stergere_baza_date(void);
 char parola_secreta[32] = "resetare_";
 int baza_date,i;
 int n = 0;
@@ -18,5 +18,19 @@ struct date{
 	char email[32];
 };
 struct date cont;
-
+//////////////////////////////////////////
+int numarareCarti();
+int verificareBiblioteca(void);
+int meniu(void);
+int adaugareCarte(void);
+int afisareLista(void);
+int cautareCarte(void);
+void grafica();
+int informatiiGrafic(void);
+struct carti{
+    char nume[32];
+    char autor[32];
+    int secol;
+};
+struct carti virtuale;
 #endif
